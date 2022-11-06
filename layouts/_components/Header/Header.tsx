692fac4067d5038b2d8a1ab718adc996a9d937e4
Milan <gergics.milan@tfsszrt.hu>
2022-11-06 18:00:15 +0100

    show counter

--- layouts/_components/Header/Header.tsx
+++ layouts/_components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import texts from "../../../plugins/Texts/texts";
+import useShoppingCartSlice from "../../../store/hooks/useShoppingCartSlice";
 import { Container, Wrapper } from "./_styles/HeaderStyles";
 
 const Header = () => {
+  const { itemCount } = useShoppingCartSlice();
+
   return (
     <Container>
       <Wrapper>
         <h1>{texts.headerTitle}</h1>
-        <p>Cart:1</p>
+        <p>Cart: {itemCount}</p>
       </Wrapper>
     </Container>
   );
