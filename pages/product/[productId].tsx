1c4c3b5ef7c255381a503c457b246e3e1d444fc9
Milan <gergics.milan@tfsszrt.hu>
2022-11-06 17:43:28 +0100

    basic product details page

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -1,9 +1,43 @@
-import { useRouter } from "next/router";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import Framed from "../../layouts/Framed";
+import paths from "../../route/paths";
 
-const ProductDetailsPage = () => {
-  const { query } = useRouter();
-  return <Framed>ProductDetails: {query.productId}</Framed>;
+type Props = {
+  product: Product;
+};
+
+const ProductDetailsPage = ({ product }: Props) => {
+  return (
+    <Framed>
+      <Link href={paths.productsList}>Products List</Link>
+      <h1>{product.title}</h1>
+      <h2>{product.category}</h2>
+      <p>{product.description}</p>
+      <Image src={product.image} alt="product_img" width={100} height={100} />
+    </Framed>
+  );
 };
 
 export default ProductDetailsPage;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const productId = context.params?.productId;
+  const jsonData = await fetch(
+    `https://fakestoreapi.com/products/${productId} `
+  );
+  const product: Product = await jsonData.json();
+
+  return { props: { product: product } };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const jsonData = await fetch("https://fakestoreapi.com/products");
+  const products: Product[] = await jsonData.json();
+  const params = products.map((p) => ({
+    params: { productId: p.id.toString() },
+  }));
+
+  return { paths: params, fallback: false };
+};
