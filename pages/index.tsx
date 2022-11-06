3a668c5c20286db1963416374429180121f69902
Milan <gergics.milan@tfsszrt.hu>
2022-11-06 16:49:28 +0100

    list products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,32 @@
+import { Grid } from "@mui/material";
+import { GetStaticProps } from "next";
+import ProductCard from "../components/ProductCard/ProductCard";
 import Framed from "../layouts/Framed";
 
-export default function ProductsListPage() {
-  return <Framed>Products</Framed>;
+type Props = {
+  products: Product[];
+};
+
+export default function ProductsListPage({ products }: Props) {
+  return (
+    <Framed>
+      <Grid container spacing={2}>
+        {products.map(({ id, title, price, image }) => (
+          <ProductCard
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            image={image}
+          />
+        ))}
+      </Grid>
+    </Framed>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const jsonData = await fetch("https://fakestoreapi.com/products");
+  const products: Product[] = await jsonData.json();
+  return { props: { products: products } };
+};
